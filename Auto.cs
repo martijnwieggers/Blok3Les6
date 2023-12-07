8c449ab4912f7a1c506728dd291734e73af76dc8
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-12-07 13:48:19 +0100

    aanpassingen

--- Auto.cs
+++ Auto.cs
@@ -8,11 +8,13 @@ namespace Blok3Les6
 {
     public class Auto
     {
-        //Stap 1: Maak hier de eigenschappen aan 
+        //Stap 1: Maak hier de eigenschappen aan.
 
-        //Stap 2: Maak hier de constructor aan waarin direct alle eigenschappen worden meegegeven
+        //Stap 2: Maak hier de constructor aan waarin direct alle eigenschappen worden meegegeven.
 
-        //Stap 3: Maak de Methode ToonInfo() aan die alle informatie afdrukt
+        //Stap 3: Maak de Methode ToonInfo() aan die alle informatie afdrukt.
+
+        //Stap 4: Maak de Methode VraagInfo() deze stelt de vragen en slaat deze op in variabelen van de class.
 
     }
 }
